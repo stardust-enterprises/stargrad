01354cb9a39eedfa7e8291cddebf0a96a24ed20a
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-04-01 19:26:57 +0200

    project: bumped to 0.5.2

--- buildSrc/src/main/kotlin/Coordinates.kt
+++ buildSrc/src/main/kotlin/Coordinates.kt
@@ -4,7 +4,7 @@ object Coordinates {
     const val repoId = "stardust-enterprises/$name"
 
     const val group = "fr.stardustenterprises"
-    const val version = "0.5.1"
+    const val version = "0.5.2"
 }
 
 object Pom {
